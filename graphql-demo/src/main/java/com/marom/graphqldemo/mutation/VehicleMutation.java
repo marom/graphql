749d8efc06a155647fc6f4834755ae4ffa79fb52
marom <maro.muszynski@gmail.com>
2019-12-21 16:41:49 +0000

    add graphql mutation (update)

---
+++ graphql-demo/src/main/java/com/marom/graphqldemo/mutation/VehicleMutation.java
@@ -0,0 +1,16 @@
+package com.marom.graphqldemo.mutation;
+
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+import com.marom.graphqldemo.dao.entity.Vehicle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class VehicleMutation implements GraphQLMutationResolver {
+
+    @Autowired
+    private VehicleService vehicleService;
+    public Vehicle createVehicle(final String type, final String modelCode, final String brandName, final String launchDate) {
+        return this.vehicleService.createVehicle(type, modelCode, brandName, launchDate);
+    }
+}
