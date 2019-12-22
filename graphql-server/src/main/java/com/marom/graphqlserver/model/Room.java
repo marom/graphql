7ba6a0ce3783774c747793d2f2ae2cac7204918b
marom <maro.muszynski@gmail.com>
2019-12-22 11:26:03 +0000

    add Room MongoDb document

---
+++ graphql-server/src/main/java/com/marom/graphqlserver/model/Room.java
@@ -0,0 +1,19 @@
+package com.marom.graphqlserver.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+public class Room {
+
+    @Id
+    private String id;
+
+    private String type;
+
+    private int occupants;
+
+    private String hotelId;
+}
