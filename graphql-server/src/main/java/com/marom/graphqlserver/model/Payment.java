a3a0ebf2004154a68964520b36b7a49d40c572d2
marom <maro.muszynski@gmail.com>
2019-12-22 11:25:01 +0000

    add Payment MongoDb document

---
+++ graphql-server/src/main/java/com/marom/graphqlserver/model/Payment.java
@@ -0,0 +1,15 @@
+package com.marom.graphqlserver.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+public class Payment {
+
+    @Id
+    private String id;
+
+    private String name;
+}
